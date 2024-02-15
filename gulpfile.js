6d994b559e27942c9e5c28e0e10e3f10fa159d89
OlozanoTamy <Olozanodeveloper@gmail.com>
2024-02-15 12:41:32 -0500

    Se realiza configuraciones finales de scss,adicion de variables,normalize,mixins, se crean primeros cambios al html-header

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,31 @@
-function tarea(done){
-    console.log('Mi primer tarea');
+function tarea(done) {
+    console.log("Mi primer tarea");
     done();
 }
 
 exports.primerTarea = tarea;
 
-
-
 const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
 //Compilar el sass a css
-function css(done){
-//    console.log("Compilando SASS...");
+function css(done) {
+    //    console.log("Compilando SASS...");
     //Identificar el archivo .scss a compilar
     src("src/scss/app.scss") //Aquí está el cambio que hice
-            .pipe( sass() ) // Compilarlo
-            .pipe( dest("build/css")); // Almacenarlo
-    
+        .pipe(plumber())
+        .pipe(sass()) // Compilarlo
+        .pipe(dest("build/css")); // Almacenarlo
+
     done();
 }
 
-function dev(done){
-    
+function dev(done) {
     watch("src/scss/**/*.scss", css);
-    
+
     done();
 }
 
 exports.css = css;
 exports.dev = dev;
+
